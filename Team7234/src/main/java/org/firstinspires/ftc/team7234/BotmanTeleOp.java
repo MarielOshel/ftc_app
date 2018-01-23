127715f8e654cdd3d6d5b6d7dad8db23e9631540
DBrown2019 <donaldbrown2019@gmail.com>
2018-01-23 17:53:28 -0500

    Initial Arm Extension +Allows for arm extension by use of gamepad 2 x

--- Team7234/src/main/java/org/firstinspires/ftc/team7234/BotmanTeleOp.java
+++ Team7234/src/main/java/org/firstinspires/ftc/team7234/BotmanTeleOp.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.team7234;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -42,9 +13,11 @@ public class BotmanTeleOp extends OpMode{
     //region Local Variable Declaration
     //Declares the power scaling of the robot
     private static final double driveCurve = 1.0;
+    private static final double extensionPow = 0.5;
     private double driveMultiplier = 1.0;
     private double armPower = 0;
     private boolean isMecanum;
+    private boolean relicExtending;
 
     private boolean mecanumToggle;
     private boolean gripperToggle;
@@ -65,6 +38,7 @@ public class BotmanTeleOp extends OpMode{
         //Controlling Booleans
         isMecanum = true;
         speedControl = false;
+        relicExtending = false;
 
         //Toggle Booleans
         mecanumToggle = true;
@@ -84,6 +58,8 @@ public class BotmanTeleOp extends OpMode{
     @Override
     public void loop() {
         //region Drive Variables
+
+        driveMultiplier = speedControl ? 0.5 : 1;
 
         //calculates angle in radians based on joystick position, reports in range [-Pi/2, 3Pi/2]
         double angle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) + (Math.PI / 2);
@@ -107,6 +83,8 @@ public class BotmanTeleOp extends OpMode{
         }
 
         //region Toggles
+
+        //region Mecanum Toggles
         if (mecanumToggle){ //Toggles drive mode based on the x button
             if (gamepad1.x){
                 isMecanum = !isMecanum;
@@ -116,7 +94,9 @@ public class BotmanTeleOp extends OpMode{
         else if (!gamepad1.x) {
             mecanumToggle = true;
         }
+        //endregion
 
+        //region Gripper Toggle
         //cycles through gripper states, once for each button press
         if (gripperToggle){
             if (gamepad2.a){
@@ -131,11 +111,19 @@ public class BotmanTeleOp extends OpMode{
         else if (!(gamepad2.a || gamepad2.b) ){
             gripperToggle = true;
         }
+        //endregion
 
-        /*if (relicToggle){ TODO: Finish this
+        if (relicToggle){
+            if (gamepad2.x){
+                relicExtending = !relicExtending;
+                relicToggle = false;
+            }
+        }
+        else if (!gamepad2.x){
+            relicToggle = true;
+        }
 
-        }*/
-
+        //region Speed Toggle
         if (speedToggle){
             if(gamepad1.b){
                 speedControl = !speedControl;
@@ -146,11 +134,13 @@ public class BotmanTeleOp extends OpMode{
             speedToggle = true;
         }
         //endregion
-        driveMultiplier = speedControl ? 0.5 : 1;
 
+        //endregion
         //endregion
 
         //region Robot Control
+
+
         //Sends Power to the Robot Arm
         robot.arm.setPower(armPower);
         //Drives the robot
@@ -177,6 +167,10 @@ public class BotmanTeleOp extends OpMode{
 
         //region Gripper Control
         robot.gripperSet(gripState);
+        //endregion
+        //region Relic Control
+        double relicPow = (relicExtending) ? extensionPow : 0;
+        robot.relicArm.setPower(relicPow);
         //endregion
 
         //region Telemetry
--- Team7234/src/main/java/org/firstinspires/ftc/team7234/HardwareBotman.java
+++ Team7234/src/main/java/org/firstinspires/ftc/team7234/HardwareBotman.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.team7234;
 
 import com.qualcomm.robotcore.hardware.ColorSensor;
@@ -50,21 +21,23 @@ public class HardwareBotman
 {
     //region Public OpMode members
 
-    DcMotor  leftFrontDrive   = null;
-    DcMotor  rightFrontDrive = null;
+    DcMotor leftFrontDrive   = null;
+    DcMotor rightFrontDrive = null;
     DcMotor leftBackDrive = null;
     DcMotor rightBackDrive = null;
-    DcMotor  arm     = null;
-    Servo    leftClaw    = null;
-    Servo    rightClaw   = null;
-    Servo jewelPusher = null;
+    DcMotor arm     = null;
+    DcMotor relicArm = null;
+    Servo   leftClaw    = null;
+    Servo   rightClaw   = null;
+    Servo   jewelPusher = null;
 
     DigitalChannel armLimit = null;
 
     ColorSensor jewelColorSensor = null;
+    //endregion
 
 
-
+    //region Values
     float hsvValues[] = {0F, 0F, 0F};
 
     public static final double MID_SERVO       =  0.5 ;
@@ -80,6 +53,7 @@ public class HardwareBotman
 
     static final double JEWEL_PUSHER_UP = 0.35;
     static final double JEWEL_PUSHER_DOWN = 0.95;
+    //endregion
 
     //Establishes variables for motors
     double[] mecanumSpeeds = {0.0, 0.0, 0.0, 0.0};
@@ -118,6 +92,7 @@ public class HardwareBotman
         leftBackDrive = hwMap.get(DcMotor.class, "left Back Drive");
         rightBackDrive = hwMap.get(DcMotor.class, "right Back Drive");
         arm    = hwMap.get(DcMotor.class, "arm");
+        relicArm = hwMap.get(DcMotor.class, "relicArm"); //TODO: Check Hardware Map on robot
 
         if (reverseRight){
             leftFrontDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
